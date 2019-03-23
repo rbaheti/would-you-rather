16279b10f75c18c332ebaa658790f9964e3e1f24
Rashmi <baheti.rash123@gmail.com>
2019-03-23 19:25:38 -0400

    refactor: Testing initial user and questions data

--- src/App.js
+++ src/App.js
@@ -1,51 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { getInitialData } from './utils/api'
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: null,
+      questions: null
+    };
+  }
+
+  componentDidMount () {
+    getInitialData()
+    .then(({ users, questions }) => {
+      this.setState({users, questions});
+    });
+  }
+
   render() {
-      return (
-        <>
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-info">Search</Button>
-      </Form>
-    </Navbar>
-    <br />
-    <Navbar bg="primary" variant="dark">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-light">Search</Button>
-      </Form>
-    </Navbar>
-  
-    <br />
-    <Navbar bg="light" variant="light">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-primary">Search</Button>
-      </Form>
-    </Navbar>
-  </>);
+
+    return (
+      <div>{JSON.stringify(this.state.users)}</div>
+    );
   }
 }
 
