acffafb4601e64f11fe23f1c49072676ca8f26d9
Rashmi <baheti.rash123@gmail.com>
2019-03-24 14:58:11 -0400

    feat: Added single use score card with the help of Bootstrap

---
+++ src/Components/UserScoreCard.jsx
@@ -0,0 +1,59 @@
+import React, {Component} from "react";
+import Card from "react-bootstrap/Card";
+import {Container, Row, Col,  ListGroup, ListGroupItem} from "react-bootstrap";
+import man from "../images/man.png";
+import boy from "../images/boy.png";
+import girl from "../images/girl.png";
+
+const getAvatar = avatar => {
+  if (avatar === "boy") return boy;
+  if (avatar === "man") return man;
+  if (avatar === "girl") return girl;
+  return "";
+};
+
+class UserScoreCard extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    const {user} = this.props;
+    const name = user[1].name;
+    const numOfAns = Object.keys(user[1].answers).length;
+    const numOfQuestions = user[1].questions.length;
+    const scores = numOfAns + numOfQuestions;
+    const image = getAvatar(user[1].avatarURL);
+
+    return (
+      <Card style={{width: "30rem"}}>
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+        </Card.Body>
+        <Container>
+          <Row>
+            <Col sm={2}>
+              <img src={image} alt="man" width="50" height="60"/>
+            </Col>
+            <Col sm={6}>
+              <ListGroup className="list-group-flush">
+                <ListGroupItem>Answered Questions: {numOfAns}</ListGroupItem>
+                <ListGroupItem>Created Questions: {numOfQuestions}</ListGroupItem>
+              </ListGroup>
+            </Col>
+            <Col sm={4}>
+              <ListGroup className="list-group-flush align-middle">
+                <ListGroupItem>Score</ListGroupItem>
+                <ListGroupItem>{scores}</ListGroupItem>
+              </ListGroup>
+            </Col>
+          </Row>
+        </Container>
+      </Card>
+    );
+  }
+}
+
+export default UserScoreCard;
