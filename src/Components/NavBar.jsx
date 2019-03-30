290c9b8c4e6cfbe8e1fef725fc86e2086359af85
Rashmi <baheti.rash123@gmail.com>
2019-03-30 14:23:59 -0400

    refactor: Wrapped bootstrap's Nav.Link with Nan.Item
    This stopped re-rendering of the entire app when a new tab was selected. Here I had also added "as={NavLink}" and replace "href" with "to" in Nav.Link react element.

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,17 +1,23 @@
 import React, {Component} from "react";
+import {NavLink} from "react-router-dom";
 import {Navbar, Nav} from "react-bootstrap";
 
 class NavBar extends Component {
-
   render() {
 
     return (
       <div>
         <Navbar bg="dark" variant="dark">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="/new">New Question</Nav.Link>
-            <Nav.Link href="/leaderboard">Leader Board</Nav.Link>
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/home">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/new">New Question</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/leaderboard">Leader Board</Nav.Link>
+            </Nav.Item>
           </Nav>
         </Navbar>
       </div>
