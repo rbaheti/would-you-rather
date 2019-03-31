1c7a0479f8f57ef208fdc61cd3c3c006d625abab
Rashmi <baheti.rash123@gmail.com>
2019-03-31 18:46:34 -0400

    fix: Fixed funtion signature from 3 parameters to an expected object

--- src/utils/api.js
+++ src/utils/api.js
@@ -19,6 +19,6 @@ export function saveQuestion(question) {
   return _saveQuestion(question);
 }
 
-export function saveQuestionAnswer(authedUser, qid, answer) {
-  return _saveQuestionAnswer(authedUser, qid, answer);
+export function saveQuestionAnswer({authedUser, qid, answer}) {
+  return _saveQuestionAnswer({authedUser, qid, answer});
 }
